6ecea43667a9b49ff3d9c7b29a669cae21aaff45
Watcharapol Watcharawisetkul <groupdoll@gmail.com>
2016-11-27 04:18:44 +0700

    Add kontractsByAddress proof of concept

--- contracts/Kontract.sol
+++ contracts/Kontract.sol
@@ -11,6 +11,8 @@ contract Kontract {
 
   Kontract[] kontracts;
 
+  mapping(address => uint[]) kontractsByAddress;
+
   function createContract(string _content, address[] _contractors, address[] _judges) returns (uint){
     var kontract = Kontract({
         creator: msg.sender,
@@ -20,11 +22,17 @@ contract Kontract {
         judges: _judges
       });
     kontracts.push(kontract);
+    uint kontractId = kontracts.length - 1;
+    kontractsByAddress[_contractors[0]].push(kontractId);
   }
 
   function getContract(uint id) returns (address creator, string content, string status, address[] contractors, address[] judges) {
     var kontract = kontracts[id];
     return (kontract.creator, kontract.content, kontract.status, kontract.contractors, kontract.judges);
+  }
+
+  function getMyContractsLength() returns (uint) {
+    return kontractsByAddress[msg.sender].length;
   }
 
 }
--- test/kontract.js
+++ test/kontract.js
@@ -8,21 +8,32 @@ contract('Kontract', function(accounts) {
     var expected_contractors = [accounts[0], accounts[1]];
     var expected_judges = [accounts[0], accounts[1], accounts[2]];
 
-    return meta.createContract(expected_content, expected_contractors, expected_judges)
-      .then(function(id) {
-        return meta.getContract.call(0).then(function (res) {
-          var creator = res[0];
-          var content = res[1];
-          var status = res[2];
-          var contractors = res[3];
-          var judges = res[4];
+    var expectedMyContractsLength = 0;
 
-          assert.equal(creator, expected_creator, "creator is invalid");
-          assert.equal(content, expected_content, "content is invalid");
-          assert.equal(status, expected_status, "status is invalid");
-          assert.deepEqual(contractors, expected_contractors, "contractors is invalid");
-          assert.deepEqual(judges, expected_judges, "judges is invalid");
-        })
+    return meta.getMyContractsLength.call()
+      .then(function (res) {
+        expectedMyContractsLength = parseInt(res);
+        return meta.createContract(expected_content, expected_contractors, expected_judges);
+      })
+      .then(function (tx_id) {
+        return meta.getMyContractsLength.call();
+      })
+      .then(function (res) {
+        assert.equal(res, expectedMyContractsLength + 1, "myContractsLength is invalid");
+        return meta.getContract.call(0);
+      })
+      .then(function (res) {
+        var creator = res[0];
+        var content = res[1];
+        var status = res[2];
+        var contractors = res[3];
+        var judges = res[4];
+
+        assert.equal(creator, expected_creator, "creator is invalid");
+        assert.equal(content, expected_content, "content is invalid");
+        assert.equal(status, expected_status, "status is invalid");
+        assert.deepEqual(contractors, expected_contractors, "contractors is invalid");
+        assert.deepEqual(judges, expected_judges, "judges is invalid");
       })
   });
 });
