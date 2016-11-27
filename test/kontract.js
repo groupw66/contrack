d0caa1a8cf906e5dbe478085b79508a4a3ce822b
Watcharapol Watcharawisetkul <groupdoll@gmail.com>
2016-11-27 13:29:27 +0700

    Update test to assert contractors and judgements

--- test/kontract.js
+++ test/kontract.js
@@ -6,14 +6,16 @@ contract('Kontract', function(accounts) {
     var expected_content = "test content";
     var expected_status = "DRAFT";
     var expected_contractors = [accounts[0], accounts[1]];
-    var expected_judges = [accounts[0], accounts[1], accounts[2]];
+    var expected_judgements = {};
+    expected_judgements[accounts[0]] = "ACCEPT";
+    expected_judgements[accounts[1]] = "EMPTY";
 
     var expectedMyContractsLength = 0;
 
     return meta.getMyContractsLength.call()
       .then(function (res) {
         expectedMyContractsLength = parseInt(res);
-        return meta.createContract(expected_content, expected_contractors, expected_judges);
+        return meta.createContract(expected_content, expected_contractors);
       })
       .then(function (tx_id) {
         return meta.getMyContractsLength.call();
@@ -29,8 +31,8 @@ contract('Kontract', function(accounts) {
         assert.equal(resObj.creator, expected_creator, "creator is invalid");
         assert.equal(resObj.content, expected_content, "content is invalid");
         assert.equal(resObj.status, expected_status, "status is invalid");
-        // assert.deepEqual(resObj.contractors, expected_contractors, "contractors is invalid");
-        // assert.deepEqual(resObj.judges, expected_judges, "judges is invalid");
+        assert.deepEqual(resObj.contractors, expected_contractors, "contractors is invalid");
+        assert.deepEqual(resObj.judgements, expected_judgements, "judgements is invalid");
       })
   });
 });
